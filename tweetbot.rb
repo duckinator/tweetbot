83e5119ad8c0696d924ddbadfc9b49c318bcde31
Nick Markwell <nick@duckinator.net>
2011-07-21 20:56:19 -0400

    Trimmed 1 char off.  $>.each -> $>.map

--- tweetbot.rb
+++ tweetbot.rb
@@ -1,1 +1,1 @@
-require'socket';a,p,c=$*;$>=TCPSocket.new a,p;puts"USER "*5,"NICK w";$>.each{|l|puts case l;when/(P.+)-/;$1+$';when/PI/;"PO#$'JOIN "+c;end}
+require'socket';a,p,c=$*;$>=TCPSocket.new a,p;puts"USER "*5,"NICK w";$>.map{|l|puts case l;when/(P.+)-/;$1+$';when/PI/;"PO#$'JOIN "+c;end}
