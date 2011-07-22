d674e97a3572c7a0d307fa7b9da8a222683e777b
Nick Markwell <nick@duckinator.net>
2011-07-21 21:01:24 -0400

    130 chars! WOO!

--- tweetbot.rb
+++ tweetbot.rb
@@ -1,1 +1,1 @@
-require'socket';a,p,c=$*;$>=TCPSocket.new a,p;puts"USER "*5,"NICK w";$>.map{|l|puts case l;when/(P.+)-/;$1+$';when/PI/;"PO#$'JOIN "+c;end}
+require'socket';a,p,c=$*;$>=TCPSocket.new a,p;puts"USER "*5,"NICK w";$>.map{|l|puts(l=~/(P.+)-/&&$1+$'||l=~/PI/&&"PO#$'JOIN "+c)}
